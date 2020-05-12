85c3fcb54f180aa3764a4e8e2a1aa7425457f5f1
Jon Uhlmann <me@jonnitto.ch>
2020-05-12 21:22:57 +0200

    :tada: First commit

---
+++ src/_flow.php
@@ -0,0 +1,18 @@
+<?php
+
+$flowhelp = file_get_contents('php://stdin');
+$flowhelp = str_replace('* ', '  ', $flowhelp);
+
+$results = array();
+foreach (preg_split('/^  (?=\w)/m', $flowhelp) as $singleLine) {
+    if (strlen(trim($singleLine)) === 0) continue;
+    // Collapse superfluous whitespace
+    $singleLine = preg_replace('/\s+/', ' ', $singleLine);
+    $tmp = explode(' ', $singleLine);
+    $command = array_shift($tmp);
+    $explanation = implode(' ', $tmp);
+
+    $results[] = sprintf('"%s:%s"', str_replace(':', '\:', $command), trim($explanation));
+}
+
+echo sprintf('cmdlist=(%s)', implode(' ', $results));
