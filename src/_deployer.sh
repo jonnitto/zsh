d21515e247daea59b2c99f9715addc2c8e97c11b
Jon Uhlmann <me@jonnitto.ch>
2020-05-14 10:31:52 +0200

    :bug: Fix deployer autocompletion

--- src/_deployer.sh
+++ src/_deployer.sh
@@ -1,15 +1,19 @@
 # Deployer basic command completion
 _deployer_get_command_list() {
-    dep --no-ansi | sed "1,/Available commands/d" | awk '/^ +[a-z]+/ { print $1 }'
+    dep --no-ansi | sed "1,/Available commands/d" | awk '/^(  )/ { print $1}'
 }
 
 _deployer_get_servers() {
-    cat deploy.yml | awk '/^[a-zA-Z]+/ { print $1 }' | tr ":" " "
+    cat deploy.yaml | awk '/^[a-zA-Z]+/ { print $1 }' | tr ":" " "
+}
+
+_deployer_get_stages() {
+    cat deploy.yaml | grep ' stage:' | awk '{ print $2 }'
 }
 
 _dep() {
     if [ "$CURRENT" = "3" ]; then
-        compadd $(_deployer_get_servers)
+        compadd $(_deployer_get_stages)
     else
         compadd $(_deployer_get_command_list)
     fi
